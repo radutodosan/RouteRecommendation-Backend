d31e94310dfd24a794c3433c5061f30da5f0d6cb
radu19 <radutodosan@yahoo.com>
2024-01-28 22:54:06 +0200

    initial commit

---
+++ src/main/java/com/routerecommendationbackend/RouteRecommendationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.routerecommendationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteRecommendationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteRecommendationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/routerecommendationbackend/RouteRecommendationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routerecommendationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteRecommendationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
