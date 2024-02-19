33965e53dff2be0c043b8a9e9451eb863db71e0b
radu19 <radutodosan@yahoo.com>
2024-02-19 22:43:27 +0200

    getting stats from completed routes only xd

--- src/main/java/com/routerecommendationbackend/repositories/StatsRepository.java
+++ src/main/java/com/routerecommendationbackend/repositories/StatsRepository.java
@@ -2,6 +2,7 @@ package com.routerecommendationbackend.repositories;
 
 import com.routerecommendationbackend.entities.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -9,5 +10,6 @@ import java.util.List;
 public interface StatsRepository extends JpaRepository<Route, Long> {
 
 
+    @Query("SELECT route FROM Route route WHERE route.user.id = :user_id AND route.status = 1")
     List<Route> getRoutesByUserId(Long user_id);
 }
