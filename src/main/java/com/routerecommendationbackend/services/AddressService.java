0ecd115907f02ca7d7a51dc5724c10d779a8ffd9
radutosan <radutodosan@yahoo.com>
2024-06-12 23:31:23 +0300

    encrypted addresses

--- src/main/java/com/routerecommendationbackend/services/AddressService.java
+++ src/main/java/com/routerecommendationbackend/services/AddressService.java
@@ -5,28 +5,57 @@ import com.routerecommendationbackend.repositories.AddressRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Base64;
+
 @Service
 @RequiredArgsConstructor
 public class AddressService {
 
     private final AddressRepository addressRepository;
+
+
+    public String decodeAddress(String encodedAddress){
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedAddress);
+        return new String(decodedBytes);
+    }
+
     public SavedAddressDTO saveAddresses(SavedAddressDTO savedAddressDTO){
 
         SavedAddressDTO updatedAddresses = this.addressRepository.findByUserId(savedAddressDTO.getUser().getId());
 
         if(updatedAddresses != null){
-            updatedAddresses.setHome(savedAddressDTO.getHome());
-            updatedAddresses.setWork(savedAddressDTO.getWork());
-            updatedAddresses.setSchool(savedAddressDTO.getSchool());
-            updatedAddresses.setOther(savedAddressDTO.getOther());
 
-            return addressRepository.save(updatedAddresses);
+            String home = Base64.getEncoder().encodeToString(savedAddressDTO.getHome().getBytes());
+            String work = Base64.getEncoder().encodeToString(savedAddressDTO.getWork().getBytes());
+            String school = Base64.getEncoder().encodeToString(savedAddressDTO.getSchool().getBytes());
+            String other = Base64.getEncoder().encodeToString(savedAddressDTO.getOther().getBytes());
+
+            updatedAddresses.setHome(home);
+            updatedAddresses.setWork(work);
+            updatedAddresses.setSchool(school);
+            updatedAddresses.setOther(other);
+
+            addressRepository.save(updatedAddresses);
+
+            updatedAddresses.setHome(decodeAddress(home));
+            updatedAddresses.setWork(decodeAddress(work));
+            updatedAddresses.setSchool(decodeAddress(school));
+            updatedAddresses.setOther(decodeAddress(other));
+
+            return updatedAddresses;
         }
 
         return addressRepository.save(savedAddressDTO);
     }
 
     public SavedAddressDTO getAddresses(Long id){
-        return addressRepository.findByUserId(id);
+        SavedAddressDTO addresses = addressRepository.findByUserId(id);
+
+        addresses.setHome(decodeAddress(addresses.getHome()));
+        addresses.setWork(decodeAddress(addresses.getWork()));
+        addresses.setSchool(decodeAddress(addresses.getSchool()));
+        addresses.setOther(decodeAddress(addresses.getOther()));
+
+        return addresses;
     }
 }
